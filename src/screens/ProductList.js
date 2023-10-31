8387898b5f6f506526ea2e0f090108836389b3ce
Vinicius <viniciusfs.contato@gmail.com>
2023-10-31 19:52:35 -0300

    atividade6_1

---
+++ src/screens/ProductList.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+export default function ProductList({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>Tela Lista der Produtos</Text>
+            <Text>Olá, você está na lista de produtos!</Text>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFC300',
+    },
+});
